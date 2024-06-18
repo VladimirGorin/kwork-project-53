371c287efa2d70266de0d435d00ae40404a7a2ee
VladimirGorin <vladimirgorin410@gmail.com>
2024-06-18 15:14:09 +0300

    fix dev mode

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import { Route, Routes, Navigate } from "react-router-dom";
-import Main from "./components/Main/Main";
-import "./App.css"
-import { useTelegram } from "./hooks/useTelegram";
-import { useEffect } from "react";
+import React from 'react'
 
-function App() {
-  const { tg, TMA } = useTelegram();
-
-  const isDevMode = false
-  const port = 8000
-
-  const api = isDevMode ? `http://localhost:${port}` : "https://api.pushcoin.world"
-
-  useEffect(() => {
-    tg?.ready()
-  }, [tg, TMA])
-
+export default function App() {
   return (
-    <div className="wrapper">
-      <Routes>
-        <Route path={"/"} element={< Navigate to={"missions"} />} />
-
-        <Route path={"/missions"} element={< Main api={api} />} />
-        <Route path={"/achievements"} element={< Main api={api} />} />
-
-      </Routes>
-    </div>
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
